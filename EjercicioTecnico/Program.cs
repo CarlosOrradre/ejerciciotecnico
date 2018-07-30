52fb38139009fa9c14060e7b7e2a3d6851b72388
CarlosOrradre <41209322+CarlosOrradre@users.noreply.github.com>
2018-07-30 10:37:59 -0300

    Ejercicio tecnico resuelto

---
+++ EjercicioTecnico/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioTecnico
+{
+    class Program
+    {
+        static List<String> RepeticionEnOraciones(List<String> lista)
+        {
+            List<string> retorno = new List<string>();
+            string fraseParametro = lista[0];
+            string fraseComparacion = lista[0];
+            string[] fraseParametroSeparada = fraseParametro.Split(CaracteresSeparacion());
+            string[] fraseActual = fraseComparacion.Split(CaracteresSeparacion());
+            int cantidadAparaciones = 0;
+            for (int i = 0; i < fraseParametroSeparada.Length; i++)
+            {
+                for (int j = 0; j <= fraseActual.Length + 1;)
+                {
+                    if (ExistePalabra(fraseParametroSeparada[i], fraseActual) && fraseParametroSeparada[i].Length > 1)
+                    {
+                        cantidadAparaciones++;
+
+                        if (cantidadAparaciones == lista.Count)
+                        {
+                            fraseComparacion = lista[0];
+                            fraseActual = fraseComparacion.Split(CaracteresSeparacion());
+                            retorno.Add(PrimeraLetraMayuscula(fraseParametroSeparada[i]));
+                            cantidadAparaciones = 0;
+                            j = 0;
+                            break;
+                        }
+                        fraseComparacion = lista[j + 1];
+                        fraseActual = fraseComparacion.Split(CaracteresSeparacion());
+                        j++;
+                    }
+                    else
+                    {
+                        fraseComparacion = lista[0];
+                        fraseActual = fraseComparacion.Split(CaracteresSeparacion());
+                        cantidadAparaciones = 0;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("Las Palabras que se repiten son: ");
+            SacarNumeros(retorno);
+            return retorno.Distinct().ToList();
+        }
+        static Boolean ExistePalabra(string palabra, String[] Frase)
+        {
+            for (int i = 0; i < Frase.Length; i++)
+            {
+                if (palabra.Equals(Frase[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static List<String> TransformarMinisculas(List<string> Frase)
+        {
+            List<String> aux = new List<string>();
+            for (int i = 0; i < Frase.Count; i++)
+            {
+                aux.Add(Frase[i].ToLower());
+            }
+            return aux;
+        }
+        static void ImprimirLista(List<String> lista)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.Write(lista[i] + " ");
+            }
+        }
+        static String PrimeraLetraMayuscula(String s)
+        {
+            s = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(s);
+            return s;
+        }
+        static Char[] CaracteresSeparacion()
+        {
+            Char[] caracteres = { ',', '¡', '!', '.', ' ', '¿', '?', };
+            return caracteres;
+        }
+        static void SacarNumeros(List<String> lista)
+        {
+            int number;
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (Int32.TryParse(lista[i], out number))
+                {
+                    lista.Remove(lista[i]);
+                }
+            }
+        }
+        static void IngresoNumeros(List<string> lista)
+        {
+            Console.WriteLine("Ingrese una oración. Ingrese 'salir' para terminar la carga y ver que palabras se repiten en las oraciones.");
+            string parametro = Console.ReadLine().Trim().ToLower();
+            while (!parametro.Equals("salir"))
+            {
+                lista.Add(parametro);
+                Console.WriteLine("Ingrese otra oración");
+                parametro = Console.ReadLine().Trim().ToLower();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<String> oraciones = new List<string>();
+            IngresoNumeros(oraciones);
+            if (oraciones.Count > 0)
+            {
+                ImprimirLista(RepeticionEnOraciones(oraciones));
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("No ha ingresado ninguna oracion");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Numeros/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros
+{
+    class Program
+    {
+        static int CantidadAparaciones(List<int> numeros, int numero)
+        {
+            int aux = 0;
+            for (int i = 0; i < numeros.Count; i++)
+            {
+                if (numeros[i] == numero)
+                {
+                    aux++;
+
+                }
+            }
+            return aux;
+        }
+        static void ImprimirLista(List<int> lista)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.Write(lista[i] + " ");
+            }
+        }
+
+        static List<int> RepetidosIgualNumero(List<int> lista)
+        {
+
+            List<int> temporal = new List<int>();
+            int apariciones = 0;
+            for (int i = 0; i < lista.Count; i++)
+            {
+                for (int j = 0; j < lista.Count; j++)
+                {
+                    if (lista[i] == lista[j])
+                    {
+                        apariciones++;
+                    }
+                }
+                if (temporal.Contains(lista[i]) != true && apariciones == lista[i])
+
+                {
+                    temporal.Add(lista[i]);
+                }
+                apariciones = 0;
+
+            }
+            return temporal;
+        }
+
+        static List<int> MayorOcurrentes(List<int> lista)
+        {
+            List<int> mayoresOcurrentes = new List<int>();
+            int cantidadaApracionesReferencia = CantidadAparaciones(lista, lista[0]);
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+
+                if (CantidadAparaciones(lista, lista[i]) > cantidadaApracionesReferencia)
+                {
+                    mayoresOcurrentes.Clear();
+                    cantidadaApracionesReferencia = CantidadAparaciones(lista, lista[i]);
+                    mayoresOcurrentes.Add(lista[i]);
+                }
+                if (CantidadAparaciones(lista, lista[i]) == cantidadaApracionesReferencia && !mayoresOcurrentes.Contains(lista[i]))
+                {
+                    cantidadaApracionesReferencia = CantidadAparaciones(lista, lista[i]);
+                    mayoresOcurrentes.Add(lista[i]);
+                }
+
+            }
+            return mayoresOcurrentes;
+        }
+
+        static List<int> MenosOcurrentes(List<int> lista)
+        {
+            List<int> menosOcurrentes = new List<int>();
+            int cantidadAparacionesReferencia = CantidadAparaciones(lista, lista[0]);
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (CantidadAparaciones(lista, lista[i]) < cantidadAparacionesReferencia)
+                {
+                    menosOcurrentes.Clear();
+                    cantidadAparacionesReferencia = CantidadAparaciones(lista, lista[i]);
+                    menosOcurrentes.Add(lista[i]);
+                }
+
+                if (CantidadAparaciones(lista, lista[i]) == cantidadAparacionesReferencia && !menosOcurrentes.Contains(lista[i]))
+                {
+                    cantidadAparacionesReferencia = CantidadAparaciones(lista, lista[i]);
+                    menosOcurrentes.Add(lista[i]);
+                }
+
+            }
+            return menosOcurrentes;
+        }
+
+        static void IngresoNumeros(List<int> lista)
+        {
+            Console.WriteLine("Ingrese un numero de uno en uno: Ingrese 'terminar' para finalizar la carga.");
+            int numero = 0;
+            string parametro = Console.ReadLine().Trim().ToLower();
+            while (!parametro.Equals("terminar"))
+            {
+                if (Int32.TryParse(parametro, out numero))
+                {
+                    lista.Add(numero);
+                }
+                else
+                {
+                    Console.WriteLine("Lo Ingresado no es un numero o intento ingresar dos numeros al mismo tiempo.");
+                }
+                Console.WriteLine("Ingrese otro numero");
+                parametro = Console.ReadLine().Trim().ToLower();
+            }
+        }
+        static void ImpresionEstructura(List<int> numeros)
+        {
+
+            Console.WriteLine();
+            Console.WriteLine("El promedio es: " + numeros.Average());
+            Console.WriteLine("Menor: " + numeros.Min() + " " + "Mayor: " + numeros.Max());
+            Console.Write("Menos Ocurrencias: " + " ");
+            ImprimirLista(MenosOcurrentes(numeros));
+            Console.Write(" ");
+            Console.Write("Mayor Ocurrencias: ");
+            ImprimirLista(MayorOcurrentes(numeros));
+            Console.Write("Repeticiones igual al número: ");
+            ImprimirLista(RepetidosIgualNumero(numeros));
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int>();
+            IngresoNumeros(numeros);
+            if (numeros.Count() != 0)
+            {
+                Console.WriteLine("La lista de números es: ");
+                ImprimirLista(numeros);
+                ImpresionEstructura(numeros);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("No ha ingresado ningún número.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
